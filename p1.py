8dd807015ebbe5c6c57faa57bbf7100ea0d4701c
sneha-m-k <sneha@deerom.com>
2021-02-16 14:42:50 +0530

    resume created

---
+++ p1.py
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Resume</title>
+</head>
+<body>
+<h1>Resume</h1>
+<h2>Sneha M K</h2>
+<img src="https://images.pexels.com/photos/850359/pexels-photo-850359.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=200">
+<p>Career Objective</p>
+<ul>
+<p>Seeking for a Software Engineer position in a fast growing organization to utilize
+expertise in software development , as well as deployment and support using agile
+development methodologies. Coming with well honed skills in computer science
+and electronics and communication in addition to excellent communication skills.</ul></p>
+<p>Qualifications</p>
+<ul>
+	<li> python internship</li>
+	<li>btech Ec</li>
+</ul>
+<p>Technical Skills</p>
+<ul>
+	<li> Python</li>
+	<li> MYSQL </li>
+	<li> HTML& CSS</li>
+	<li> Django</li>
+	<li> Electronics and communication</li>
+</ul>
+<p>Certifications</p>
+<ul>
+	<h5>MTA Introduction to python by Microsoft</h5>
+	<h5>Python Hacke Rank</h5>
+	<h5>Problem Solving Hacker Rank</h5>
+	<h5>Business English Cambridge(BEC)</h5>
+</ul>
+<p>Hobbies</p>
+<ul>
+	<li> Watching TED talks</li>
+	<li> Listening music</li>
+	<li> Swimming</li>
+	<li> Playing violin</li>
+
+</ul>
+
+</body>
+</html>
